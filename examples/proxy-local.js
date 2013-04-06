5fda34270960858de3231aaec199a43138ca79bd
tom zhou <zs68j2ee@gmail.com>
2013-04-06 16:02:54 +0800

    added proxy-local.js example

---
+++ examples/proxy-local.js
@@ -0,0 +1,19 @@
+var Proxy = require('../index');
+var http = require('http');
+
+// create local http server
+var server = htpp.createServer(function(req, res){
+    res.writeHeader(200);
+    res.write('Hello, this is local http server.');
+    res.end();
+});
+
+server.listen(3000);
+console.log('Local http server listening on port 3000');
+
+var proxy = new Proxy(['http://localhost:3000/'], function(err, proxyURL){
+        console.log('Website                            Proxy URL(please open it on browser)');
+        for (var k in proxyURL) {
+            console.log(k+'        '+proxyURL[k]);
+        }
+    });
