ffdbdce1fcd596fb866b8c08c148abb8b5978663
tom zhou <zs68j2ee@gmail.com>
2013-04-16 00:29:36 +0800

    support non utf-8 charst text/html rewrite, bump version to 0.0.12

--- examples/proxy-remote.js
+++ examples/proxy-remote.js
@@ -1,6 +1,6 @@
 var Proxy = require('../index');
 
-var srv = new Proxy(['http://w3schools.com/'], function(err, proxyURL){
+var srv = new Proxy(['http://www.w3school.com.cn/'], function(err, proxyURL){
         console.log('Website                            Proxy URL(please open it on browser)');
         for (var k in proxyURL) {
             console.log(k+'        '+proxyURL[k]);
