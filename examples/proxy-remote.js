ce21a0385b15a1b474cc661f9c2b0d16a68643f6
tom zhou <zs68j2ee@gmail.com>
2013-10-19 18:07:54 +0800

    fixed text/html rewrite res.headers.content-length

--- examples/proxy-remote.js
+++ examples/proxy-remote.js
@@ -1,6 +1,6 @@
 var Proxy = require('../index');
 
-var srv = new Proxy(['https://github.com/', 'http://www.w3school.com.cn/'], function(err, proxyURL){
+var srv = new Proxy(['http://www.google.com.hk/', 'http://www.w3school.com.cn/'], function(err, proxyURL){
         console.log('Website                            Proxy URL(please open it on browser)');
         for (var k in proxyURL) {
             console.log(k+'        '+proxyURL[k]);
