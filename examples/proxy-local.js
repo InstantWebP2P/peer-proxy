d2dab4ddf49f3b92387090dda0e7505867915ae7
tom zhou <zs68j2ee@gmail.com>
2013-10-31 08:51:04 +0800

    fixed typo

--- examples/proxy-local.js
+++ examples/proxy-local.js
@@ -11,7 +11,7 @@ var server = http.createServer(function(req, res){
 server.listen(3000);
 console.log('Local http server listening on port 3000');
 
-var proxy = new Proxy(['http://192.188.1.168:8081/'], function(err, proxyURL){
+var proxy = new Proxy(['http://localhost:3000/'], function(err, proxyURL){
         console.log('Website                            Proxy URL(please open it on browser)');
         for (var k in proxyURL) {
             console.log(k+'        '+proxyURL[k]);
